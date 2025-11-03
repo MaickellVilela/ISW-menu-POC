fe4f21f31ade1105f9ad76651e5ca8f628f6df3d
Maickell Vilela <maickell@me.com>
2025-11-03 11:19:37 -0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-900 text-white min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold">Menu POC</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ['~/assets/css/tailwind.css'],
+  tailwindcss: {
+    config: {
+      content: [
+        './components/**/*.{js,vue,ts}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.{js,ts}',
+        './app.vue',
+        './error.vue',
+      ],
+    },
+  },
+})
